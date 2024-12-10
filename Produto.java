0ea79cb07a561ced7ea2c0205a795136c0a818b9
Lucas Pinheiro <165809855+Pinheiro-Lucasz@users.noreply.github.com>
2024-12-10 15:07:29 -0400

    Add files via upload

---
+++ Produto.java
@@ -0,0 +1,38 @@
+package Reflections;
+
+public class Produto {
+
+    private long codigo;
+
+    private String descricao;
+
+    private Double valor;
+
+    public Produto(){
+
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+}
